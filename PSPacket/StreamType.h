32791888f44bcf71a06ea1ee0da957813869b1a7
unknown <leizhang49@iflytek.com>
2018-11-16 17:43:01 +0800

    添加PS流类型的定义

---
+++ PSPacket/StreamType.h
@@ -0,0 +1,12 @@
+/**
+ * PS包中的流类型取值
+ */
+
+#define MPEG4_VIDEO 0x10
+#define H264_VIDEO 0x1B
+#define SVAV_VIDEO 0x80
+#define G711_AUDIO 0x90
+#define G7221_AUDIO 0x92
+#define G7231_AUDIO 0x93
+#define G729_AUDIO 0x99
+#define SVAC_AUDIO 0x9B
